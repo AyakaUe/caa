13b2cd6cae57b6c76b2054bc62ed1a8d9ad32e3f
上 彩加 <83008807@bandainamco.co.jp>
2026-06-05 09:34:53 +0900

    :bug: fix 勤務地自動チェック処理 null対応

--- BloomMaker/caa_apply_form/action/1. 初期表示/1.2.3. 台帳テーブルのデータを設定/勤務地チェックボックス設定.js
+++ BloomMaker/caa_apply_form/action/1. 初期表示/1.2.3. 台帳テーブルのデータを設定/勤務地チェックボックス設定.js
@@ -1,5 +1,10 @@
-
-// DBの値を画面表示用に変換
-$variable.work_location_cheked.forEach(i => {
-    i.isChecked = $variable.optionalParameter.userParameter.caa_t_apply_info.work_location.split(",").includes(i.item_cd);
-});
+// 勤務地nullチェック　エラーハンドリング
+if ($variable.optionalParameter.userParameter.caa_t_apply_info.work_location !== null && $variable.optionalParameter.userParameter.caa_t_apply_info.work_location !== "") {
+    // DBの値を画面表示用に変換
+    $variable.work_location_cheked.forEach(i => {
+        i.isChecked = $variable.optionalParameter.userParameter.caa_t_apply_info.work_location.split(",").includes(i.item_cd);
+    });
+    // 勤務地その他チェック
+    $variable.is_work_location_other_checked
+        = $variable.work_location_cheked.filter(i => i.isChecked && i.item_cd === "99").length > 0;
+}
