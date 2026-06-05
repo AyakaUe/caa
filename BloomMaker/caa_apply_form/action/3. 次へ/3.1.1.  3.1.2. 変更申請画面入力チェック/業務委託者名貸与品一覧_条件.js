c240eeaa7ff3935a0c0e09be2eb8e6aa1b9763f7
上 彩加 <83008807@bandainamco.co.jp>
2026-06-05 09:31:52 +0900

    :bug: fix 貸与品一覧入力チェックnull対応

--- BloomMaker/caa_apply_form/action/3. 次へ/3.1.1.  3.1.2. 変更申請画面入力チェック/業務委託者名貸与品一覧_条件.js
+++ BloomMaker/caa_apply_form/action/3. 次へ/3.1.1.  3.1.2. 変更申請画面入力チェック/業務委託者名貸与品一覧_条件.js
@@ -5,46 +5,51 @@ const name_regex = /^.+[ \u3000]+.+$/;
 const kana_regex = /^(?:[\u3040-\u309Fー]+|[A-Za-z]+)(?:[ \u3000]+(?:[\u3040-\u309Fー]+|[A-Za-z]+))+$/;
 
 for (var i = 0; i < $variable.optionalParameter.userParameter.caa_t_contractor_provided_item.length; i++) {
-    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_name == "") {
+    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_name == ""
+        || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_name == null) {
         return true;
     } else if (!$variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_name.match(name_regex)) {
         return true;
     }
     // アカウント有の場合はよみがな必須
     if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_account_flg == "01") {
-        if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana == "") {
-            console.log("contract_user_kana is null");
+        if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana == ""
+            || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana == null) {
             return true;
         } else if (!$variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana.match(kana_regex)) {
-            console.log("contract_user_kana is invalid");
             return true;
         }
     } else {
         if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana != ""
             && !$variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].contract_user_kana_no_check.match(kana_regex)) {
-            console.log("contract_user_kana_no_check is invalid");
             return true;
         }
     }
-    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_account_flg == "") {
+    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_account_flg == ""
+        || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_account_flg == null) {
         return true;
     }
-    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_pc_flg == "") {
+    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_pc_flg == ""
+        || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_pc_flg == null) {
         return true;
     }
-    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_id_card_flg == "") {
+    if ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_id_card_flg == ""
+        || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_id_card_flg == null) {
         return true;
     }
     if ($input.provided_dev_equipment_flg[0].is_disable == $env.const.false
-        && $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_dev_equipment_flg == "") {
+        && ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_dev_equipment_flg == ""
+            || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_dev_equipment_flg == null)) {
         return true;
     }
     if ($input.provided_mobile_flg[0].is_disable == $env.const.false
-        && $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_mobile_flg == "") {
+        && ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_mobile_flg == ""
+            || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_mobile_flg == null)) {
         return true;
     }
     if ($input.provided_folder_access_flg[0].is_disable == $env.const.false
-        && $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_folder_access_flg == "") {
+        && ($variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_folder_access_flg == ""
+            || $variable.optionalParameter.userParameter.caa_t_contractor_provided_item[i].provided_folder_access_flg == null)) {
         return true;
     }
 }
