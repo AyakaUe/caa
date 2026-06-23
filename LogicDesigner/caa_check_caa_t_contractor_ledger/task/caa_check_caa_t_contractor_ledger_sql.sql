2c3f210cfed1e316216e3ff329de193d41d0abb2
上 彩加 <83008807@bandainamco.co.jp>
2026-06-23 19:46:06 +0900

    :bug: 課題管理表No.120対応 台帳更新チェックを管理IDから管理番号で見るように変更

--- LogicDesigner/caa_check_caa_t_contractor_ledger/task/caa_check_caa_t_contractor_ledger_sql.sql
+++ LogicDesigner/caa_check_caa_t_contractor_ledger/task/caa_check_caa_t_contractor_ledger_sql.sql
@@ -1,10 +1,8 @@
 --*DataTitle '業務委託者管理台帳トラン'
 --*Captions 'バージョン'
 SELECT
-    version 
+    MAX(version) as version
 FROM
     PUBLIC.caa_t_contractor_ledger 
 WHERE
-    manage_id = /*manage_id*/
-ORDER BY
-    manage_id
+    manage_no = /*manage_no*/''
